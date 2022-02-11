4776ce1e43d1db3a8cf637eb71292c73864bc8ce
KenjiRN <kenji_nakajima@hotmail.com>
2022-02-11 16:23:51 -0400

    h

--- index2.php
+++ index2.php
@@ -1,12 +1,45 @@
 <?php 
-    echo "Olá!";
+    echo "Olá!";echo "\n";
     $idade1 = 21;
     echo $idade1;
-
+    echo "\n";
   
 
-$idade = 22;
+// =======================================================
+
+$idade = 25;
+
+echo $idade; echo "\n";
+// ======================================================
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+$doisAoCubo = 2 ** 3;
+
+$restoDaDivisao = 10 % 3;
+
+echo $soma;echo "\n";
+echo $subtracao;echo "\n";
+echo $multiplicacao;echo "\n";
+echo $divisao;echo "\n";
+echo $doisAoCubo;echo "\n";
+echo $restoDaDivisao;echo "\n";
+
+// ==========================================================
+
 
 $idade = 21;
+$salario = 1000.301;
+$divisao = 10 / 3;
 
-echo $idade;
+$divisao = 3;
+
+$texto = "Olá mundo";
+
+$verdadeiro = true;
+$falso = false;
+
+echo gettype($verdadeiro);echo "\n";
