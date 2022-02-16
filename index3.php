691df8c8af118f1e100199246fad427cfa301291
KenjiRN <kenji_nakajima@hotmail.com>
2022-02-16 16:41:24 -0400

    update

--- index3.php
+++ index3.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "Hello World";
+// echo "Hello World";
+
+// $n1 = 1;
+// $n2 = 2;
+// // $tipo = $_GET["op"];
+// echo "Os valores passados foram $n1 e $n2";
+// ==========================================================
+// $ano = 2010;
+// $idade = 2022 - $ano;
+// echo "Quem nasceu em $ano tem idade de $idade. ";
+// $tipo = ($idade >=18 && $idade <= 65) ?  "OBRIGATORIO" : "NÃO OBRIGATORIO";
+// echo "E dessa forma seu foto é $tipo";
+//   ===================================================================
+
+$a = 3;
+$b = "3";
+$r = ($a === $b) ? "SIM" : "NAO";
+echo "As variaveis A e B sao iguais? $r";
